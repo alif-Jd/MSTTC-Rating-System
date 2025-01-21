c872e10980d7f8ed100df93853606c8d65f46f58
alif-Jd <alifjd2004@gmail.com>
2025-01-21 10:04:29 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
 $servername = "localhost"; // Use "localhost" for Hostinger; otherwise, check Hostinger's control panel for the specific hostname
-$username = "u927582686_msttc_db"; // Your database username from Hostinger
-$password = "msttc_dbConnect04"; // Your database password from Hostinger
-$dbname = "u927582686_msttc_db"; // Your database name from Hostinger
+$username = ""; // Your database username from Hostinger
+$password = ""; // Your database password from Hostinger
+$dbname = ""; // Your database name from Hostinger
 
 // Create connection
 $connect = new mysqli($servername, $username, $password, $dbname);
